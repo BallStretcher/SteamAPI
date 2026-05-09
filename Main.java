17b16c8b913ae3b47e2b59f4394263b700f9059b
Artem Landakov <81481064+BallStretcher@users.noreply.github.com>
2026-05-09 21:37:30 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+public class Main {
+    public static void main(String[] args) {
+
+        PlayerStat me = new PlayerStat("76561198145570899");
+        PlayerStat.getAllInfo(me);
+        //AllGamesStat s = new AllGamesStat("76561198145570899");
+        //AllGamesStat.getGamesInfo("76561198145570899");
+
+
+    }
+}
